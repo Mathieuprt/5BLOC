9a12fbfa555c88318ede212004b9e41eabffa421
Mathieuprt <mathieu.perrot06@gmail.com>
2026-01-31 14:31:19 +0100

    Modifs gitignore

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const CONTRACT_ABI = [
     "function balanceOf(address account, uint256 id) view returns (uint256)",
     "function adminMint(address account, uint256 id, uint256 amount)",
